5219fe7f8106ec57c364a2610ab18b8ddd52dc0c
Blair McKenzie <blair@daemon.com.au>
2024-09-27 14:17:33 +1000

    update base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://shib71.github.io/concerto-log-analyzer/",
 })
